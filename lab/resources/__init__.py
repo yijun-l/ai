6d505e86f1424264ba2956303452e40a099349b8
Yijun <buglittlej@gmail.com>
2026-06-26 21:55:44 +0800

    lab(ml): cost function

---
+++ lab/resources/__init__.py
@@ -0,0 +1,2 @@
+from .lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+from .lab_utils_common import dlblue, dlorange, dldarkred, dlmagenta, dlpurple, dlcolors, compute_cost
